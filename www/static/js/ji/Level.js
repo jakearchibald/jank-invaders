b925b40668ab6b375cffac90e7ef5d45f34fa2f2
Jake Archibald <jaffathecake@gmail.com>
2013-04-30 07:26:48 +0100

    Avoid freezing on ship placement

--- www/static/js/ji/Level.js
+++ www/static/js/ji/Level.js
@@ -17,13 +17,16 @@
 
   LevelProto.play = function() {
     var ship;
+    var shipPlacementTries = 1000; // stops us getting in an infinite loop
 
     for (var i = this.normalShips + this.jankyShips; i--;) {
       ship = ji.Ship.random();
       do {
         ship.y = Math.random() * (this._canvas.height - ship.height);
         ship.x = -ship.width - Math.random() * this._canvas.width;
-      } while (this._getIntersectingShip(ship.x, ship.y, ship.width, ship.height));
+
+        if (shipPlacementTries) { shipPlacementTries--; }
+      } while (shipPlacementTries && this._getIntersectingShip(ship.x, ship.y, ship.width, ship.height));
       ship.xMax = this._canvas.width;
       ship.xVel = this.speed + (Math.random() * (this.speedVariance * 2)) - this.speedVariance;
       ship.jankiness = i < this.jankyShips ? this.jankiness : 0;
@@ -37,7 +40,7 @@
 
   LevelProto._getIntersectingShip = function(x, y, width, height) {
     var ship;
-    for (var i = this._ships.length; i--;) { // loop backwards to get on-top ship
+    for (var i = this._ships.length; i--;) { // loop backwards to get topmost ship
       ship = this._ships[i];
       if (
         x < ship.x + ship.width &&
